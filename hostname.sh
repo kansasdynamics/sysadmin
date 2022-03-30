1faabc389d9b399d1254078a39a6280a369656b8
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-03-29 18:44:52 -0700

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -2,3 +2,7 @@
 # Change the hostname of the system
 read -p "Enter Hostname: " HOSTNAME
 hostnamectl set-hostname $HOSTNAME
+hostname
+# This can be accomplished with multiple methods
+# nmtui (GUI)
+# nmcli general hostname $HOSTNAME
