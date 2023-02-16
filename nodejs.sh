62abcc7361c575764c03a7a85e517e3570cd4870
Alex <89443340+kansasdynamics@users.noreply.github.com>
2023-02-15 18:26:35 -0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -6,3 +6,5 @@ sudo apt-get install nodejs npm -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 command -v nvm
 nvm install 14.17.6
+npm install npm@6.14.15
+npm install
